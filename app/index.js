a5ecd0e9459c61ad09e42b19cf7b7e6afbe11727
klequis <klequis@gmail.com>
2017-03-19 20:16:35 -0700

    chapter complete

--- app/index.js
+++ app/index.js
@@ -3,6 +3,8 @@ import 'font-awesome/css/font-awesome.css';
 import 'purecss';
 import './main.css';
 import component from './component';
+import { bake } from './shake';
 
+bake();
 
 document.body.appendChild(component());
